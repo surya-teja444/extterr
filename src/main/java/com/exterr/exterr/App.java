1915904caece974e3e0634586dbc863836344d8c
suryachandra <suryachandrateja444@gmail.com>
2022-12-29 00:07:40 +0530

    mass

---
+++ src/main/java/com/exterr/exterr/App.java
@@ -0,0 +1,13 @@
+package com.exterr.exterr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello !" );
+    }
+}
